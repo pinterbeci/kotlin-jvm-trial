06556e8847c40c0de3d7bb43ce4a85659df0f395
pinterbeci <pinter.beci99@gmail.com>
2025-12-29 09:35:02 +0100

    sanitize gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,23 +1,20 @@
 plugins {
-    kotlin("jvm") version "2.0.0" // Kotlin version to use
+    kotlin("jvm") version "2.0.0"
 }
 
-group = "hu.pinter.beci.kotlin.documentation" // A company name, for example, `org.jetbrains`
-version = "1.0.0-SNAPSHOT" // Version to assign to the built artifact
+group = "hu.pinterbela.kotlin.documentation"
+version = "1.0.0-SNAPSHOT"
 
-repositories { // Sources of dependencies. See 1️⃣
-    mavenCentral() // Maven Central Repository. See 2️⃣
+repositories {
+    mavenCentral()
 }
 
-dependencies { // All the libraries you want to use. See 3️⃣
-    // Copy dependencies' names after you find them in a repository
-    testImplementation(kotlin("test")) // The Kotlin test library
-    // https://mvnrepository.com/artifact/org.assertj/assertj-core
+dependencies {
+    testImplementation(kotlin("test"))
     testImplementation("org.assertj:assertj-core:3.6.1")
-    // https://mvnrepository.com/artifact/org.jetbrains.kotlin/kotlin-stdlib
     implementation(kotlin("stdlib"))
 }
 
-tasks.test { // See 4️⃣
-    useJUnitPlatform() // JUnitPlatform for tests. See 5️⃣
+tasks.test {
+    useJUnitPlatform()
 }
